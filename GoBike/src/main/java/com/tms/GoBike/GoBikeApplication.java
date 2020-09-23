3a58fce1e49ce9b8ac7d925eb04609a7516cbf8f
Will Boland <will@fyrstertech.com>
2020-09-23 11:47:01 -0400

    adds GoBike gradle springboot project

---
+++ GoBike/src/main/java/com/tms/GoBike/GoBikeApplication.java
@@ -0,0 +1,13 @@
+package com.tms.GoBike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoBikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoBikeApplication.class, args);
+	}
+
+}
---
+++ GoBike/src/test/java/com/tms/GoBike/GoBikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tms.GoBike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoBikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
