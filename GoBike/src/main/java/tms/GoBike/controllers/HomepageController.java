babab1b8e8d4cc04e7d67f952657ef6f3667a19c
Will Boland <will@fyrstertech.com>
2020-09-26 01:59:18 -0400

    Adds initial login setup and service

---
+++ GoBike/src/main/java/tms/GoBike/controllers/HomepageController.java
@@ -0,0 +1,12 @@
+package tms.GoBike.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomepageController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
---
+++ GoBike/src/main/java/tms/GoBike/security/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package tms.GoBike.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/restricted").authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.userDetailsService(new BikerLoginDetailsService());
+    }
+}
