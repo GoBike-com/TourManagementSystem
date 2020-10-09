9bffeffff80eb49694b5903382a166f3e1a311ef
jbhushan791 <bhushan.jyoti791@gmail.com>
2020-10-09 09:49:39 -0400

    P565P14-34 : Added tables for Place and Image

---
+++ GoBike/src/main/java/com/iu/gobike/model/Image.java
@@ -0,0 +1,40 @@
+package com.iu.gobike.model;
+
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.sql.Blob;
+import java.time.Instant;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Builder
+@Entity
+@Table
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "IMAGE")
+    private Blob image;
+
+    @JoinColumn(name = "PLACE_ID")
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Place place;
+
+    @Column(name="CREATED_DATE", updatable = false)
+    @CreatedDate
+    private Instant createdDate;
+
+    @Column(name="LAST_MODIFIED_DATE")
+    @LastModifiedDate
+    private Instant lastModifiedDate;
+
+}
