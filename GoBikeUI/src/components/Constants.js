92ebdfdfdfc478da2b31359d5442d17787408675
Will Boland <will@fyrstertech.com>
2020-10-26 17:33:03 -0400

    fix constants.js

--- GoBikeUI/src/components/Constants.js
+++ GoBikeUI/src/components/Constants.js
@@ -1,10 +1,7 @@
 // Constants.js
-const prod = {
+const urls = {
+    // API_URL: 'http://localhost:7070'
     API_URL: 'http://gobike.us-east-2.elasticbeanstalk.com' //uses port 5000
 };
 
-const dev = {
-    API_URL: 'http://localhost:7070'
-};
-
-export const config = process.env.NODE_ENV === 'development' ? dev : prod;
+export const config = urls;
