0c8d050d9c6156a443016d3b247d7cb84dab5930
jbhushan791 <bhushan.jyoti791@gmail.com>
2020-10-26 18:51:31 -0400

    P565P14-45 : Added API to get flight details for given search criteria from Amadeus

--- GoBike/src/main/java/com/iu/gobike/service/TravelServiceImpl.java
+++ GoBike/src/main/java/com/iu/gobike/service/TravelServiceImpl.java
@@ -2,14 +2,11 @@ package com.iu.gobike.service;
 
 import com.iu.gobike.dto.SearchAirportResponse;
 import com.iu.gobike.dto.SearchFlightRequest;
-import com.iu.gobike.dto.SearchFlightResponse;
+import com.iu.gobike.amadeus.dto.SearchFlightAmadeusResponse;
 import com.iu.gobike.util.AmadeusRestTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 /**
  * @author jbhushan
@@ -23,10 +20,20 @@ public class TravelServiceImpl implements TravelService {
     @Override
     public SearchAirportResponse searchAirports(String keyword) {
         ResponseEntity<SearchAirportResponse> r = amadeusRestTemplate.get("v1/reference-data/locations?subType=CITY&countryCode=US&view=LIGHT&keyword="+keyword, SearchAirportResponse.class);
+        return r.getBody();
+    }
 
-        HttpHeaders responseHeaders = new HttpHeaders();
-        responseHeaders.set("Baeldung-Example-Header",
-                "Value-ResponseEntityBuilderWithHttpHeaders");
+    @Override
+    public SearchFlightAmadeusResponse searchFlights(SearchFlightRequest request) {
+        String url = "v2/shopping/flight-offers?currencyCode=USD&originLocationCode=" + request.getSource() +
+                "&destinationLocationCode=" + request.getDestination() + "&departureDate=" + request.getTravelDate() +
+                "&adults=" + request.getAdults()+"&nonStop="+request.isNonStop()+"&travelClass="+request.getTravelClass().name();
+        String returnDate = request.getReturnDate();
+        if(returnDate!=null){
+            url = url+"&returnDate="+returnDate;
+        }
+        ResponseEntity<SearchFlightAmadeusResponse> r = amadeusRestTemplate.get(url, SearchFlightAmadeusResponse.class);
+        //TODO: Jyoti convert into goBike response
         return r.getBody();
     }
 }
