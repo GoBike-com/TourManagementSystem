bb8105b079ca972dcd7e186e1d5ad60e4edbf0bb
Will Boland <will@fyrstertech.com>
2020-10-25 21:37:19 -0400

    Trying out card styles with material ui for explore page

--- GoBikeUI/src/components/Explore/Explore.js
+++ GoBikeUI/src/components/Explore/Explore.js
@@ -1,115 +1,116 @@
 import React from "react";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import { fade, makeStyles } from "@material-ui/core/styles";
-import DirectionsBikeIcon from "@material-ui/icons/DirectionsBike";
-import { Grid, Paper } from "@material-ui/core";
-import { Link , withRouter } from "react-router-dom";
-import Button from "../../assets/components/CustomButtons/Button.js";
-import CssBaseline from "@material-ui/core/CssBaseline";
+import {makeStyles} from "@material-ui/core/styles";
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 import ListSubheader from '@material-ui/core/ListSubheader';
 import IconButton from '@material-ui/core/IconButton';
+import Divider from '@material-ui/core/Divider';
 import InfoIcon from '@material-ui/icons/Info';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import CImage from '../../assets/img/chicago.jpg';
+import Header from "./Header";
 
 class Explore extends React.Component {
-    tileData = [
-           {
-                 img: "a",
-                 title: 'Image',
-                author: 'author',
-              },
-        {
-            img: "a",
-            title: 'Image',
-            author: 'author',
-        },
- ];
-
     constructor(props) {
         super(props);
-        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+    thingsToDoData = [
+        {
+            id: 1,
+            img: CImage,
+            title: 'Image',
+            description: 'author',
+            cols: 2,
+        },
+        {
+            id: 2,
+            img: CImage,
+            title: 'Image',
+            description: 'author',
+            cols: 1
+        },
+        {
+            id: 3,
+            img: CImage,
+            title: 'Image',
+            description: 'author',
+            cols: 1,
+        },
+        {
+            id: 4,
+            img: CImage,
+            title: 'Image',
+            description: 'author',
+            cols: 1,
+        },
+        {
+            id: 5,
+            img: CImage,
+            title: 'Image',
+            description: 'author',
+            cols: 1,
+        },
+        {
+            id: 6,
+            img: CImage,
+            title: 'Image',
+            description: 'author',
+            cols: 1,
+        },
+        {
+            id: 7,
+            img: CImage,
+            title: 'Image',
+            description: 'author',
+            cols: 2,
+        },
+    ];
+    tileData = [
+        {
+            img: CImage,
+            title: 'Image',
+            description: 'author',
+        },
+        {
+            img: CImage,
+            title: 'Image',
+            description: 'author',
+        },
+        {
+            img: CImage,
+            title: 'Image',
+            description: 'author',
+        },
+        {
+            img: CImage,
+            title: 'Image',
+            description: 'author',
+        },
+    ];
+
+    //Styles
     useStyles = makeStyles((theme) => ({
-        root: {
-            flexGrow: 2
-        },
-        menuButton: {
-            marginRight: theme.spacing(2),
-        },
-        title: {
-            flexGrow: 1,
-            display: "none",
-            [theme.breakpoints.up("sm")]: {
-                display: "block",
-            },
-        },
-        search: {
-            position: "relative",
-            borderRadius: theme.shape.borderRadius,
-            backgroundColor: fade(theme.palette.common.white, 0.15),
-            "&:hover": {
-                backgroundColor: fade(theme.palette.common.white, 0.25),
-            },
-            marginLeft: 0,
-            width: "100%",
-            [theme.breakpoints.up("sm")]: {
-                marginLeft: theme.spacing(1),
-                width: "auto",
-            },
-        },
-        searchIcon: {
-            padding: theme.spacing(0, 2),
-            height: "100%",
-            position: "absolute",
-            pointerEvents: "none",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-        },
-        inputRoot: {
-            color: "inherit",
-        },
-        logo: {
-            color: "green",
-            click: "cursor",
-        },
-        inputInput: {
-            padding: theme.spacing(1, 1, 1, 0),
-            // vertical padding + font size from searchIcon
-            paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-            transition: theme.transitions.create("width"),
-            width: "100%",
-            [theme.breakpoints.up("sm")]: {
-                width: "12ch",
-                "&:focus": {
-                    width: "20ch",
-                },
-            },
-        },
         placeTitle: {
             width: '100%',
             maxWidth: 500,
             textDecoration: 'underline'
         },
-        paper: {
-            padding: theme.spacing(2),
-            margin: 'auto',
-            maxWidth: 500,
-        },
-        image: {
-            width: 128,
-            height: 128,
-        },
-        img: {
-            margin: 'auto',
-            display: 'block',
-            maxWidth: '100%',
-            maxHeight: '100%',
+        gridRoot: {
+            display: 'flex',
+            flexWrap: 'wrap',
+            justifyContent: 'space-around',
+            overflow: 'hidden',
+            backgroundColor: theme.palette.background.paper,
+            maxWidth: '75%'
         },
         gridList: {
             width: 500,
@@ -118,84 +119,133 @@ class Explore extends React.Component {
         icon: {
             color: 'rgba(255, 255, 255, 0.54)',
         },
-        gridRoot: {
-            display: 'flex',
-            flexWrap: 'wrap',
-            justifyContent: 'space-around',
-            overflow: 'hidden',
-            backgroundColor: theme.palette.background.paper,
-        }
     }));
     classes = this.useStyles;
-
-    handleSubmit = (event) => {
-        event.preventDefault();
-        const targetUrl = "http://localhost:7070/user/logout"; //call backend to add to itinerary
-
-        fetch(targetUrl,
-            {
-                method:'get',
-                credentials: 'include',
-                headers: {'Content-Type': 'application/json', Accept: 'application/json'},
-            }
-        ).then(
-            response => {
-                if (response.status == "200") {
-                    alert("Added to itinerary!")
-                }
-            })
-            .catch(error => {
-                console.error('There was an error!', error);
-            });
-    };
-
 
     render() {
         return (
             <div>
-            <Grid>
-                <CssBaseline />
-                <div className={this.classes.root}>
-                    <AppBar position="static" style={{ backgroundColor: "indigo" }}>
-                        <Toolbar>
-                            <Typography
-                                className={this.classes.title} variant="h6" noWrap style={{ fontSize: "24px", marginLeft:"275px", paddingRight:"800px" }}>
-                                GoBike
-                                <DirectionsBikeIcon className={this.classes.logo} />
-                            </Typography>
-                            <Link style={{float:"right"}}>
-                                <Button size="sm" style={{alignItems:"right", marginRight:"10px", }} onClick= {this.handleSubmit}>
-                                    Add to Itinerary
-                                </Button>
-                            </Link>
-                        </Toolbar>
-                    </AppBar>
-                </div>
+                {/*Top Bar*/}
+                <Header/>
+
+                {/*Place Name*/}
                 <Typography className={this.classes.placeTitle} variant="h1" component="h2" gutterBottom align="center">
                     Chicago
                 </Typography>
-            </Grid>
+
+                {/*Short Place Description*/}
+                <Typography variant="body1" gutterBottom align={"center"}>
+                    Chicago, on Lake Michigan in Illinois, is among the largest cities in the U.S. Famed for its bold architecture, it has a skyline punctuated by skyscrapers such as the iconic John Hancock Center, 1,451-ft. Willis Tower (formerly the Sears Tower) and the neo-Gothic Tribune Tower. The city is also renowned for its museums, including the Art Institute of Chicago with its noted Impressionist and Post-Impressionist works.
+                </Typography>
+                <Divider variant="middle" />
+
+                {/*Things to Do*/}
                 <div className={this.classes.gridRoot}>
-                <GridList cellHeight={180} className={this.classes.gridList}>
-                    <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
-                        <ListSubheader component="div">Things to Do</ListSubheader>
-                    </GridListTile>
-                    {this.tileData.map((tile) => (
-                        <GridListTile key={tile.img}>
-                            <img src={tile.img} alt={tile.title} />
-                            <GridListTileBar
-                                title={tile.title}
-                                subtitle={<span>{tile.author}</span>}
-                                actionIcon={
-                                    <IconButton aria-label={`info about ${tile.title}`} className={this.classes.icon}>
-                                        <InfoIcon />
-                                    </IconButton>
-                                }
-                            />
+                    <GridList cellHeight={300} className={this.classes.gridList} cols={3}>
+                        <GridListTile key="Subheader" cols={3} style={{height: 'auto'}}>
+                            <ListSubheader component="div">Things to Do</ListSubheader>
                         </GridListTile>
-                    ))}
-                </GridList>
+                        {this.thingsToDoData.map((tile) => (
+                            <GridListTile key={tile.img} cols={tile.cols || 1}>
+                                <img src={tile.img} alt={tile.title}/>
+                                <GridListTileBar
+                                    title={tile.title}
+                                    subtitle={<span>{tile.description}</span>}
+                                    actionIcon={
+                                        <IconButton aria-label={`info about ${tile.title}`} className={this.classes.icon}>
+                                            <InfoIcon/>
+                                        </IconButton>
+                                    }
+                                />
+                            </GridListTile>
+                        ))}
+                    </GridList>
                 </div>
+
+                {/*Restaurants*/}
+                <GridList cellHeight={300} className={this.classes.gridList} cols={3}>
+                    <GridListTile key="Subheader" cols={3} style={{height: 'auto'}}>
+                        <ListSubheader component="div">Restaurants</ListSubheader>
+                    </GridListTile>
+                </GridList>
+                <Grid container spacing={5}>
+                    {this.thingsToDoData.map((tile) => (
+                        <Grid item md={3}>
+                            <Card className={this.classes.root}>
+                                <CardActionArea>
+                                    <CardMedia
+                                        className={this.classes.media}
+                                        image={tile.img}
+                                        title="Contemplative Reptile"
+                                    />
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="h2">
+                                            Lizard
+                                        </Typography>
+                                        <Typography variant="body2" color="textSecondary" component="p">
+                                            {tile.description}
+                                        </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                                <CardActions>
+                                    <Button size="small" color="primary">
+                                        Share
+                                    </Button>
+                                    <Button size="small" color="primary">
+                                        Learn More
+                                    </Button>
+                                </CardActions>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
+
+                {/*Hotels*/}
+                <div className={this.classes.gridRoot}>
+                    <GridList cellHeight={300} className={this.classes.gridList}>
+                        <GridListTile key="Subheader" cols={2} style={{height: 'auto'}}>
+                            <ListSubheader component="div">Hotels</ListSubheader>
+                        </GridListTile>
+                        {this.tileData.map((tile) => (
+                            <GridListTile key={tile.img}>
+                                <img src={tile.img} alt={tile.title}/>
+                                <GridListTileBar
+                                    title={tile.title}
+                                    subtitle={<span>{tile.description}</span>}
+                                    actionIcon={
+                                        <IconButton aria-label={`info about ${tile.title}`}
+                                                    className={this.classes.icon}>
+                                            <InfoIcon/>
+                                        </IconButton>
+                                    }
+                                />
+                            </GridListTile>
+                        ))}
+                    </GridList>
+                </div>
+                {/*Flights*/}
+                {/*<div className={this.classes.gridRoot}>*/}
+                {/*    <GridList cellHeight={300} className={this.classes.gridList}>*/}
+                {/*        <GridListTile key="Subheader" cols={2} style={{height: 'auto'}}>*/}
+                {/*            <ListSubheader component="div">Things to Do</ListSubheader>*/}
+                {/*        </GridListTile>*/}
+                {/*        {this.tileData.map((tile) => (*/}
+                {/*            <GridListTile key={tile.img}>*/}
+                {/*                <img src={tile.img} alt={tile.title}/>*/}
+                {/*                <GridListTileBar*/}
+                {/*                    title={tile.title}*/}
+                {/*                    subtitle={<span>{tile.description}</span>}*/}
+                {/*                    actionIcon={*/}
+                {/*                        <IconButton aria-label={`info about ${tile.title}`}*/}
+                {/*                                    className={this.classes.icon}>*/}
+                {/*                            <InfoIcon/>*/}
+                {/*                        </IconButton>*/}
+                {/*                    }*/}
+                {/*                />*/}
+                {/*            </GridListTile>*/}
+                {/*        ))}*/}
+                {/*    </GridList>*/}
+                {/*</div>*/}
             </div>
         );
     };
