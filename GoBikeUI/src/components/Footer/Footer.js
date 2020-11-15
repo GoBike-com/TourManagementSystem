373b2407901e7ad286a099ae5725663e39a9e216
jbhushan791 <bhushan.jyoti791@gmail.com>
2020-11-15 17:01:38 -0500

    P565P14-102 : Sticked footor to the bottom

--- GoBikeUI/src/components/Footer/Footer.js
+++ GoBikeUI/src/components/Footer/Footer.js
@@ -30,6 +30,10 @@ const useStyles = makeStyles((theme) => ({
     footer: {
         padding: theme.spacing(3, 2),
         marginTop: 'auto',
+        position: 'absolute',
+        bottom: 0,
+        width: '100%',
+        
         backgroundColor:
             theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800]
     },
